aa4484dbf666847eb09b49eefc74e9e38be7ef5f
Michael B. Resnick <mresnick@resnick.io>
2020-10-16 16:17:06 -0400

    ch3

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,6 +7,19 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            List<double> grades = new List<double>() { 12.7, 10.3, 6.11, 4.1 };
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+
+            result /= grades.Count;
+
+            Console.WriteLine($"The average grade is {result:N1}");
+
             if(args.Length > 0)
             {
                 Console.WriteLine($"Hello, {args[0]}!");
